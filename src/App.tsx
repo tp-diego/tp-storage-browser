f919b1b49e583b67e1c93f8c63a05f4009b7b297
tp-diego <diego.guerrero@teleperformance.com>
2025-06-26 11:05:06 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,52 @@ import {
   defineComponentTheme,
 } from '@aws-amplify/ui-react/server';
 
-Amplify.configure(config);
+//Amplify.configure(config);
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "eu-central-1_ArLgyUPsX",
+      userPoolClientId: "7tbhlf3jbpsk5hks9bc1gkduvk",
+      identityPoolId: "eu-central-1:f500e5d5-187e-427e-b06b-a17a41197bae",
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: false,
+      passwordFormat: {
+        minLength: 12,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+  Storage: {
+    S3: {
+      bucket: "systems-amplify-test",
+      region: "eu-central-1",
+      // default bucket metadata should be duplicated below with any additional buckets
+      buckets: {
+        "systems-amplify-test": {
+          bucketName: "systems-amplify-test",
+          region: "eu-central-1",
+          paths: {
+            "systems/*": {
+              authenticated: ["get", "list"],
+              //groupsadmin: ["get", "list", "write", "delete"],
+            },
+          }
+        }
+      }
+    }
+  }
+});
 
 const storageBrowserTheme = defineComponentTheme({
   name: 'storage-browser',
