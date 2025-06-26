f05ab432e5700387eef3dee2d3ee5a3ab7885c36
tp-diego <diego.guerrero@teleperformance.com>
2025-06-26 10:35:24 +0200

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,4 @@
-import { defineBackend } from '@aws-amplify/backend';
+/*import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { storage } from './storage/resource';
 
@@ -6,7 +6,45 @@ import { storage } from './storage/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+/*defineBackend({
   auth,
   storage
 });
+*/
+
+import { defineBackend } from "@aws-amplify/backend";
+import { Effect, Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { auth } from "./auth/resource";
+
+const backend = defineBackend({
+  auth,
+});
+
+const customBucketStack = backend.createStack("custom-bucket-stack");
+
+// Import existing bucket
+const customBucket = Bucket.fromBucketAttributes(customBucketStack, "MyCustomBucket", {
+  bucketArn: "arn:aws:s3:::systems-amplify-test",
+  region: "eu-central-1"
+});
+
+backend.addOutput({
+  storage: {
+    aws_region: customBucket.env.region,
+    bucket_name: customBucket.bucketName,
+    buckets: [
+      {
+        aws_region: customBucket.env.region,
+        bucket_name: customBucket.bucketName,
+        name: customBucket.bucketName,
+        paths: {
+          "systems/*": {
+            authenticated: ["get", "list"],
+          },
+        },
+      }
+    ]
+  },
+});
+
