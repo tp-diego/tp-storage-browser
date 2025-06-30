ef879fac705f13cd57389aaf9d10f8270abd440a
Diego Guerrero Estevez <estevez.10@emea.teleperformance.com>
2025-06-30 11:19:34 +0200

    añadido bucket

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,7 +2,7 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'systems-amplify-test',
-  isDefault: true,
+  isDefault: false,
    access: (allow) => ({
     'systems/*': [
         //allow.authenticated.to(['read', 'write', 'delete'])
