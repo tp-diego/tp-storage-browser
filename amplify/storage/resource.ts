85aafec54b2195ada7a56fc40b8b5522160eaded
tp-diego <diego.guerrero@teleperformance.com>
2025-07-01 10:43:53 +0200

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,12 +1,12 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'systems-amplify-test',/*
+  name: 'systems-amplify-test'/*,
   isDefault: false,
    access: (allow) => ({
     'systems/*': [
         //allow.authenticated.to(['read', 'write', 'delete'])
         allow.groups(['systems']).to(['read', 'write', 'delete']),
-    ]*/
-   })
+    ]
+   })*/
 });
