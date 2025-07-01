7fca4663a5501e9db5dc4d84ab744ba5b672c3b6
tp-diego <diego.guerrero@teleperformance.com>
2025-07-01 10:39:11 +0200

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,6 +2,7 @@ import { defineBackend } from "@aws-amplify/backend";
 import { Effect, Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 import { auth } from "./auth/resource";
+import { storage } from './storage/resource';
 
 const backend = defineBackend({
   auth,
